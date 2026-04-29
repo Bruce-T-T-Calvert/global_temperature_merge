5f25f162f1860ca0afe6167b927bbcde3db55e02
Bruce-T-T-Calvert <160699906+Bruce-T-T-Calvert@users.noreply.github.com>
2026-04-29 01:24:21 -0400

    Add files via upload

--- gmst_merge/translators/noaaglobaltemp_v51.py
+++ gmst_merge/translators/noaaglobaltemp_v51.py
@@ -1,18 +1,28 @@
 from pathlib import Path
-import os
+import numpy as np
+import sys
+sys.path.append(str(Path(__file__).resolve().parent.parent))
 
+data_file_dir = os.getenv('DATADIR')
+if data_file_dir is None:
+    data_file_dir = Path(__file__).resolve().parent.parent / 'Data' / 'NOAAGlobalTempv5.1'
+else:
+    data_file_dir = data_file_dir / 'ManagedData' / 'Data' / 'NOAAGlobalTempv5.1'
 
-def convert_file():
-    data_dir_env = os.getenv('DATADIR')
-    DATA_DIR = Path(data_dir_env)
+# softcoded filename
+matches = sorted(Path(data_file_dir).glob("aravg.ann.land_ocean.90S.90N.*.asc"))
 
-    data_file_dir = DATA_DIR / 'ManagedData' / 'Data' / 'NOAA v5.1'
-    filename = data_file_dir / 'aravg.ann.land_ocean.90S.90N.v5.1.0.202312.asc'
+file = open(matches[-1])
+years = np.zeros((0,1))
+mean = np.zeros((0,1))
+while True:
+    words = file.readline().split()
+    if len(words) == 0:
+        break
+    years = np.append(years,float(words[0]))
+    mean = np.append(mean,float(words[1]))
+file.close()
+years = years.reshape(-1,1)
+mean = mean.reshape(-1,1)
 
-    with open(data_file_dir / 'ensemble_time_series.csv', 'w') as o:
-        with open(filename, 'r') as f:
-            for line in f:
-                columns = line.split()
-                columns = columns[0:2]
-                line = ','.join(columns) + '\n'
-                o.write(line)
+np.savetxt(data_file_dir / "ensemble_time_series.csv", np.concatenate((years,mean),axis=1), fmt='%.16f', delimiter=",")
